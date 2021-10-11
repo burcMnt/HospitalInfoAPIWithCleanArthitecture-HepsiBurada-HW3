18178a653f14267f24e9c456b762933135b29a97
burcMnt <burcumantar0@gmail.com>
2021-10-11 17:47:31 +0300

    Add project files.

---
+++ src/ApplicationCore/Entities/Doctor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ApplicationCore.Entities
+{
+    public class Doctor : BaseEntity
+    {
+        public string LastName { get; set; }
+
+        public string Gender { get; set; }
+
+        public string Clinic { get; set; }
+
+        public int HospitalId { get; set; }
+        public virtual Hospital Hospital { get; set; }
+
+        public virtual ICollection<DoctorPatient> DoctorPatients { get; set; }
+    }
+}
